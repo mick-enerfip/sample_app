38477a8b1487fe9f28cb9999dc33c2875997fc01
Michael <mick@enerfip.fr>
2016-12-30 10:22:34 +0100

    lot of change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,8 @@ class PagesController < ApplicationController
         def about
               @titre = "À Propos"
                 end
+        
+        def help
+          @titre = "Aide"
+        end
 end
